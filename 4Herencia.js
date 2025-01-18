39b29fcc1d0287295cd23ab0ce1365bd80eee7d3
Jasson-01 <jason.davila001@gmail.com>
2025-01-18 19:31:04 -0300

    Agregando la clase 4 - Herencia

---
+++ 4Herencia.js
@@ -0,0 +1,42 @@
+class Animal {
+    constructor(nombre,sonido){
+        this.nombre = nombre 
+        this.sonido = sonido 
+    }
+    
+    hacerSonido(){
+        console.log(`${this.nombre} hace: ${this.sonido}`)
+    }
+}
+
+class Perro extends Animal {
+    constructor(nombre){
+        // La palabra reservada "super" hacer referencia al llamado del constructor del Padre del que hereda
+        super(nombre,"Guau")
+    }
+
+    moverCola(){
+        console.log(`${this.nombre} está moviendo la cola felizmente`)
+    }
+}
+
+class Gato extends Animal {
+    constructor(nombre){
+        super(nombre,"Miau")
+    }
+
+    ronronear(){
+       console.log(`${this.nombre} está ronroneando contento`)
+
+    }
+}
+
+const perrito = new Perro("Manchita")
+const gatito = new Gato("Pelusa")
+
+// Hacer sonido lo puedo usar en ambos ya que heredan de Animal
+perrito.hacerSonido()
+perrito.moverCola()
+
+gatito.hacerSonido()
+gatito.ronronear()
